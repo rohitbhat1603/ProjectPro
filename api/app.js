1223b5fee348a0088d7896e9bb4324daca0dd652
Rohit Bhat <80664968+rohitbhat1603@users.noreply.github.com>
2023-07-14 01:13:18 +0530

    Update app.js

--- api/app.js
+++ api/app.js
@@ -8,12 +8,10 @@ import fs from 'fs';
 import path from 'path';
 import 'dotenv/config';
 
-const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
